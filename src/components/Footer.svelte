a62a84d84239f01e1a5573a9e4536d3c9d123963
kasey.culmback <kasey.culmback@aon.com>
2020-07-18 16:53:13 -0700

    add footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,21 @@
+<div class="text-center my-6">
+  <p>
+    Code available on
+    <a
+      href="https://github.com/kculmback/url-shortener"
+      target="github"
+      class="text-green-500 font-bold hover:underline"
+      rel="noopener nofollow noreferrer">
+      Github
+    </a>
+  </p>
+  <p>
+    Built by
+    <a
+      href="https://kaseyculmback.com"
+      target="kc"
+      class="text-green-500 font-bold hover:underline">
+      Kasey Culmback
+    </a>
+  </p>
+</div>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,11 +1,16 @@
 <script>
   import GlobalStyle from "../components/GlobalStyle.svelte";
+  import Footer from "../components/Footer.svelte";
 </script>
 
 <GlobalStyle />
 
-<main>
-  <div class="container mx-auto my-8">
+<main class="h-full flex flex-col">
+  <div class="container mx-auto my-8 flex-grow flex-shrink-0">
     <slot />
   </div>
+  <div class="px-8">
+    <hr class="my-4 mx-auto max-w-lg" />
+  </div>
+  <Footer />
 </main>
